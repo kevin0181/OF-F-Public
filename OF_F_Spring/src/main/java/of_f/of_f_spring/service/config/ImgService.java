d4afdf583fdf838748c071e1229b8e76f30548cc
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2022-11-30 18:48:59 +0900

    이미지 저장 완료

--- OF_F_Spring/src/main/java/of_f/of_f_spring/service/config/ImgService.java
+++ OF_F_Spring/src/main/java/of_f/of_f_spring/service/config/ImgService.java
@@ -5,54 +5,49 @@ import of_f.of_f_spring.domain.entity.user.EmailToken;
 import of_f.of_f_spring.domain.exception.ApiException;
 import of_f.of_f_spring.domain.exception.ExceptionEnum;
 import of_f.of_f_spring.dto.store.menu.StoreMenuDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.UUID;
 
 
 @Service
 public class ImgService {
+    @Value("${img.connect.path}")
+    private String fileDir;
+
     public StoreMenuImg saveMenuImg(StoreMenuDTO storeMenuDTO, MultipartFile imgFile) throws IOException {
 
-        String imgName = null;
-        String imagePath = null;
-        String absolutePath = "";
-        String originalFileExtension = null;
-        String path = "images/menu";
-        File file = new File(path);
+        File file = new File(fileDir);
         if (!file.exists()) {
             file.mkdirs();
         }
 
-        String contentType = imgFile.getContentType();
-        if (ObjectUtils.isEmpty(contentType)) {
-            throw new ApiException(ExceptionEnum.NOT_IMG);
-        } else {
-            if (contentType.contains("image/jpeg")) {
-                originalFileExtension = ".jpg";
-            } else if (contentType.contains("image/png")) {
-                originalFileExtension = ".png";
-            } else {
-                throw new ApiException(ExceptionEnum.NOT_IMG);
-            }
-        }
+        // 원래 파일 이름 추출
+        String origName = imgFile.getOriginalFilename();
 
-        imgName = createRandomToken() + "_" + notDate();
-        file = new File(imgName);
-//        try {
-        imgFile.transferTo(file);
-//        } catch (IOException e) {
-//            throw
-//        }
+        // 파일 이름으로 쓸 uuid 생성
+        String uuid = UUID.randomUUID().toString();
+
+        // 확장자 추출(ex : .png)
+        String extension = origName.substring(origName.lastIndexOf("."));
+
+        // uuid와 확장자 결합
+        String savedName = uuid + extension;
+
+        // 파일을 불러올 때 사용할 파일 경로
+        String savedPath = fileDir + savedName;
+
+        // 실제로 로컬에 uuid를 파일명으로 저장
+        imgFile.transferTo(new File(savedPath));
+
         return null;
     }
 
