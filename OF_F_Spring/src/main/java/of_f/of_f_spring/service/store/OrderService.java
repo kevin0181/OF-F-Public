b9c1216236c66bdb5b6992c961bef7251689e07b
유영빈(집) <kevin018111@gmail.com>
2022-12-15 18:57:08 +0900

    order 저장 테스트코드 까지 완료

--- OF_F_Spring/src/main/java/of_f/of_f_spring/service/store/OrderService.java
+++ OF_F_Spring/src/main/java/of_f/of_f_spring/service/store/OrderService.java
@@ -3,6 +3,10 @@ package of_f.of_f_spring.service.store;
 import of_f.of_f_spring.domain.entity.store.Store;
 import of_f.of_f_spring.domain.entity.store.order.StoreOrder;
 import of_f.of_f_spring.domain.entity.store.qr.StoreQRId;
+import of_f.of_f_spring.domain.exception.OrderException;
+import of_f.of_f_spring.domain.exception.OrderExceptionEnum;
+import of_f.of_f_spring.domain.exception.StoreException;
+import of_f.of_f_spring.domain.exception.StoreExceptionEnum;
 import of_f.of_f_spring.domain.mapper.store.OrderMapper;
 import of_f.of_f_spring.domain.mapper.store.StoreMapper;
 import of_f.of_f_spring.dto.response.ApiResponseDTO;
@@ -15,7 +19,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class OrderService {
@@ -30,7 +37,6 @@ public class OrderService {
 
     @Transactional
     public ApiResponseDTO getStoreMenuList(Long storeSeq) {
-        List<StoreOrder> storeOrderList = storeOrderRepository.findAll();
         Store store = storeRepository.findById(storeSeq).orElse(null);
         StoreDTO storeDTO = StoreMapper.instance.storeToStoreDTOByOrderUser(store);
         return ApiResponseDTO.builder()
@@ -44,8 +50,13 @@ public class OrderService {
 
         StoreQRId storeQRId = storeQRIdRepository.findByQrId(qrId);
 
+        if (storeQRId == null)  // qr id 없음.
+            throw new OrderException(OrderExceptionEnum.DOES_NOT_EXIST_QR_ID);
+
+
         StoreOrder storeOrder = OrderMapper.instance.storeOrderDTOToStoreOrder(storeOrderDTO);
 
+        storeOrder.setId(randomOrderId());
         storeOrder.setStoreQRIdSeq(storeQRId.getSeq());
 
         StoreOrderDTO resStoreOrderDTO =
@@ -58,5 +69,33 @@ public class OrderService {
                 .build();
     }
 
+    public String randomOrderId() {
+
+        StringBuffer temp = new StringBuffer();
+        Random rnd = new Random();
+        for (int i = 0; i < 5; i++) {
+            int rIndex = rnd.nextInt(3);
+            switch (rIndex) {
+                case 0:
+                    // a-z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    break;
+                case 1:
+                    // A-Z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    break;
+                case 2:
+                    // 0-9
+                    temp.append((rnd.nextInt(10)));
+                    break;
+            }
+        }
+
+        Date today = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyMMdd.HHmm");
+        String result = df.format(today);
+
+        return result + "." + temp;
+    }
 
 }
