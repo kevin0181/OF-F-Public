99d02993d3009f6d89e7bdc118d2620091c81376
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2022-12-23 14:58:18 +0900

    react header css 초기본 됨

--- of_f_react/src/components/header/Header.js
+++ of_f_react/src/components/header/Header.js
@@ -1,6 +1,7 @@
 import "./../../styles/css/header/header.css";
 import logo2 from "./../../assets/logo2.svg";
 import logo1 from "./../../assets/logo1.svg";
+import startIcon from "./../../assets/icon/start_icon.svg";
 
 let Header = () => {
     return (
@@ -16,10 +17,24 @@ let Header = () => {
                 </div>
                 <div className={"nav"}>
                     <ul>
-                        <li>홈</li>
-                        <li>소개</li>
-                        <li>구독료</li>
+                        <li>
+                            <a href="#!">홈</a>
+                        </li>
+                        <li>
+                            <a href="#!">소개</a>
+                        </li>
+                        <li>
+                            <a href="#!">구독료</a>
+                        </li>
                     </ul>
+                </div>
+                <div className={"auth-btn"}>
+                    <div>
+                        <div>
+                            <img src={startIcon} alt={"start icon"}/>
+                        </div>
+                        <div>시작하기</div>
+                    </div>
                 </div>
             </div>
         </div>
