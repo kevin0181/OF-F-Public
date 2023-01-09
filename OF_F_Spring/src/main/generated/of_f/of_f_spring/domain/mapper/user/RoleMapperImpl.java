2a44e1fe0395011e2da44314525f9d0c2649b4de
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-01-09 14:50:02 +0900

    관리자 로그인 성공 확인

--- OF_F_Spring/src/main/generated/of_f/of_f_spring/domain/mapper/user/RoleMapperImpl.java
+++ OF_F_Spring/src/main/generated/of_f/of_f_spring/domain/mapper/user/RoleMapperImpl.java
@@ -8,7 +8,7 @@ import of_f.of_f_spring.dto.user.RoleDTO;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-09T11:47:28+0900",
+    date = "2023-01-09T14:39:56+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 1.8.0_292 (AdoptOpenJDK)"
 )
 public class RoleMapperImpl implements RoleMapper {
--- of_f_react/src/management/ManagementLogin.js
+++ of_f_react/src/management/ManagementLogin.js
@@ -1,8 +1,85 @@
 import "./../styles/css/management/managementLogin.css";
 import logo from "./../assets/logo1.svg";
 import userLogo from "./../assets/icon/user.svg";
+import {notTokenAxios} from "../Config/customAxios";
+import {useEffect, useState} from "react";
+import {useCookies} from "react-cookie";
+import {useNavigate} from "react-router-dom";
 
 let ManagementLogin = () => {
+
+    const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (localStorage.getItem("l-st") === "true") { //이미 로그인 되어 있는 상태라면?
+            navigate("/");
+        }
+    }, []);
+
+    const [auth, setAuth] = useState({
+        email: "",
+        password: ""
+    });
+
+    let onChangeAuth = (e) => {
+        setAuth({
+            ...auth,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    let onKeyPressLogin = (e) => {
+        if (e.key === "Enter") {
+            loginBtn();
+        }
+    }
+
+    let loginBtn = () => {
+
+        if (auth.email === "") {
+            alert("이메일을 입력하세요.");
+            return;
+        }
+
+        if (auth.password === "") {
+            alert("비밀번호를 입력하세요.");
+            return;
+        }
+
+
+        notTokenAxios({
+            method: 'post',
+            url: '/api/v1/auth/n/login',
+            data: {
+                email: auth.email,
+                password: auth.password
+            }
+        }).then(res => {
+
+            if (res.data.code === 401) { //로그인 실패
+                alert(res.data.detail);
+                return;
+            }
+
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 30);
+
+            setCookie("accessToken", res.data.data.accessToken, {
+                path: "/",
+                expires
+            });
+
+            localStorage.setItem("l-st", true); // -> 로그인 된 상태
+            window.location.replace("/");
+
+        }).catch((err) => {
+            console.log(err);
+            alert(err.response.data.detail);
+        });
+    }
+
     return (<>
         <div className={"management-container"}>
             <div className={"management-login-form"}>
@@ -11,15 +88,18 @@ let ManagementLogin = () => {
                 </div>
                 <div className={"management-login-form-body"}>
                     <div className={"management-login-form-div"}>
-                        <input className={"m-input"} placeholder={"email"}/>
+                        <input className={"m-input"} name={"email"} value={auth.email}
+                               onChange={onChangeAuth}
+                               placeholder={"email"}/>
                     </div>
                     <div className={"management-login-form-div"}>
-                        <input className={"m-input"} placeholder={"password"}/>
+                        <input className={"m-input"} onChange={onChangeAuth} onKeyPress={onKeyPressLogin} type={"password"}
+                               name={"password"} value={auth.password} placeholder={"password"}/>
                     </div>
                 </div>
                 <div className={"management-login-form-bottom"}>
                     <div className={"manage-login-btn"}>
-                        <div className={"login-btn"}>
+                        <div className={"login-btn"} onClick={loginBtn}>
                             <img src={userLogo} style={{
                                 width: "20%",
                                 height: "90%",
