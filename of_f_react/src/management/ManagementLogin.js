c88e899c60bef366d98c962da4f5b16145f6ca35
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-01-09 13:49:02 +0900

    관리자 로그인 페이지 만듬

--- of_f_react/src/management/ManagementLogin.js
+++ of_f_react/src/management/ManagementLogin.js
@@ -1,10 +1,28 @@
 import "./../styles/css/management/managementLogin.css";
+import logo from "./../assets/logo1.svg";
 
 let ManagementLogin = () => {
     return (<>
         <div className={"management-container"}>
-            <div className={"login-form"}>
-
+            <div className={"management-login-form"}>
+                <div className={"management-login-form-top"}>
+                    <img src={logo} className={"manage-login-logo"}/><h2>관리자 로그인</h2>
+                </div>
+                <div className={"management-login-form-body"}>
+                    <div className={"management-login-form-div"}>
+                        <input className={"m-input"} placeholder={"email"}/>
+                    </div>
+                    <div className={"management-login-form-div"}>
+                        <input className={"m-input"} placeholder={"password"}/>
+                    </div>
+                </div>
+                <div className={"management-login-form-bottom"}>
+                    <div className={"manage-login-btn"}>
+                        <div className={"login-btn"}>
+                            <p>로그인</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>);
