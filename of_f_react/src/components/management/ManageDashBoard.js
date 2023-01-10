e4bc4104820de6ce5cec6fe1e0dcc6802b8e7ed9
유영빈(집) <kevin018111@gmail.com>
2023-01-10 18:10:53 +0900

    기본 권한 주는거 순서 바꿨음

--- of_f_react/src/components/management/ManageDashBoard.js
+++ of_f_react/src/components/management/ManageDashBoard.js
@@ -1,6 +1,16 @@
 import "./../../styles/css/management/manageDashBoard.css";
+import {useQuery} from "../../Config/getQuery";
+import {useEffect, useState} from "react";
 
 let ManageDashBoard = () => {
+    const query = useQuery();
+
+    const [kindStatus, setKindStatus] = useState("");
+
+    useEffect(() => {
+        setKindStatus(query.get("kind"));
+    }, [query]);
+
     return (<>
         <div className={"manage-dash-board-container"}>
         </div>
