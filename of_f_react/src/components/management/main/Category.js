8b59ee8bc48fa55a61a4173ac7b01a8bd3bb21c0
유영빈(집) <kevin018111@gmail.com>
2023-01-11 19:32:46 +0900

    일단 상태에 따라서 보이는거 다르게 만들어둠

--- of_f_react/src/components/management/main/Category.js
+++ of_f_react/src/components/management/main/Category.js
@@ -1,9 +1,18 @@
+import "./../../../styles/css/management/main.css";
 import "./../../../styles/css/management/category.css";
+import {useQuery} from "../../../Config/getQuery";
+import {useNavigate} from "react-router-dom";
+
 
 let Category = () => {
+
+    const query = useQuery();
+
+    const navigate = useNavigate();
+
     return (
         <div className={"manage-main-container"}>
-            <div className={"t-line f-line m-scroll"}>
+            <div className={"f-line m-scroll "}>
                 <div>
                     <div className={"name-card"}>
 
@@ -19,16 +28,27 @@ let Category = () => {
                     </div>
                 </div>
             </div>
-            <div className={"t-line c-line m-scroll"}>
-                <div>
+            {
+                query.get("c") !== null ? (
+                    <div
+                        className={"c-line m-scroll animate__animated " + (query.get("c") !== null ? 'animate__slideInLeft' : '')}>
+                        <div>
 
-                </div>
-            </div>
-            <div className={"t-line l-line m-scroll"}>
-                <div>
+                        </div>
+                    </div>) : (
+                    <></>
+                )
+            }
+            {
+                query.get("l") !== null ? (
+                    <div
+                        className={"l-line m-scroll animate__animated " + (query.get("l") !== null ? 'animate__slideInLeft' : '')}>
+                        <div>
 
-                </div>
-            </div>
+                        </div>
+                    </div>
+                ) : (<></>)
+            }
         </div>
     )
 }
