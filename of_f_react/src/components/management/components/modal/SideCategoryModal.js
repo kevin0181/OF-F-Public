e624d2a909a831a46e09750b0d309b5a80e31672
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-01-18 20:19:11 +0900

    사이드 카테고리 모달창 만들었음

--- of_f_react/src/components/management/components/modal/SideCategoryModal.js
+++ of_f_react/src/components/management/components/modal/SideCategoryModal.js
@@ -1,8 +1,22 @@
 import "./../../../../styles/css/management/sideModal.css";
+import {ReactComponent as XBtn} from "./../../../../assets/icon/x.svg";
+import {useNavigate} from "react-router-dom";
+import {useQuery} from "../../../../Config/getQuery";
+
 let SideCategoryModal = () => {
+
+    const navigate = useNavigate();
+    const query = useQuery();
+
     return (
         <div className={"side-modal"}>
-
+            <div className={"side-modal-container animate__animated animate__backInLeft"}>
+                <div className={"side-modal-close-btn"} onClick={() => {
+                    navigate(`/manage/store?kind=${query.get("kind")}&f=${query.get("f")}&status=${query.get("status")}`);
+                }}>
+                    <XBtn/>
+                </div>
+            </div>
         </div>
     )
 }
