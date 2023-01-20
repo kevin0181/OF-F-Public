b9800c8a59fce7abddbb9fa53e56a88b9d5cb93a
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-01-20 11:51:12 +0900

    메뉴 상세보기 컴포넌트로 데이터 가져오는거함

--- of_f_react/src/components/management/components/menu/MenuDetail.js
+++ of_f_react/src/components/management/components/menu/MenuDetail.js
@@ -1,18 +1,19 @@
-import {useEffect, useState} from "react";
-import {tokenStoreAdminAxios} from "../../../../Config/customStoreAdminAjax";
 import {useRecoilState} from "recoil";
 import {selectStoreInfoRecoil} from "../../../../store/management/storeInfo";
 import {useQuery} from "../../../../Config/getQuery";
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {tokenStoreAdminAxios} from "../../../../Config/customStoreAdminAjax";
 
-let CategoryDetail = ({category}) => {
+let MenuDetail = ({menu}) => {
+
 
     const [store, setStore] = useRecoilState(selectStoreInfoRecoil);
     const query = useQuery();
 
     const navigate = useNavigate();
 
-    const [categoryDetail, setCategoryDetail] = useState({
+    const [menuDetail, setMenuDetail] = useState({
         seq: "",
         name: "",
         status: false,
@@ -21,22 +22,26 @@ let CategoryDetail = ({category}) => {
     });
 
     useEffect(() => {
-        if (category !== undefined) {
-            setCategoryDetail(category);
+        if (menu !== undefined) {
+            setMenuDetail(menu);
         }
-    }, [category]);
+    }, [menu]);
+
+    useEffect(() => {
+        console.log(menuDetail);
+    }, [menuDetail]);
 
     let onChangeCategoryDetail = (e) => {
-        setCategoryDetail({
-            ...categoryDetail,
+        setMenuDetail({
+            ...menuDetail,
             [e.target.name]: e.target.value
         })
     }
 
     let onChangeCategoryStatusToggle = (e) => {
-        setCategoryDetail({
-            ...categoryDetail,
-            status: !categoryDetail.status
+        setMenuDetail({
+            ...menuDetail,
+            status: !menuDetail.status
         })
     }
 
@@ -45,7 +50,7 @@ let CategoryDetail = ({category}) => {
         tokenStoreAdminAxios({
             method: "POST",
             url: "/api/v1/store/admin/category?status=update",
-            data: categoryDetail
+            data: menuDetail
         }).then(res => {
             let resData = res.data.data;
             let updateCategories = [...store.storeCategories];
@@ -66,11 +71,11 @@ let CategoryDetail = ({category}) => {
 
     let onClickCategoryDelete = () => {
         // eslint-disable-next-line no-restricted-globals
-        if (confirm(categoryDetail.name + "를 삭제하시겠습니까?")) {
+        if (confirm(menuDetail.name + "를 삭제하시겠습니까?")) {
             tokenStoreAdminAxios({
                 method: "POST",
                 url: "/api/v1/store/admin/category?status=delete",
-                data: categoryDetail
+                data: menuDetail
             }).then(res => {
                 let deleteAfterCategories = store.storeCategories.filter(data => {
                     return data.seq !== res.data.data.seq;
@@ -94,17 +99,16 @@ let CategoryDetail = ({category}) => {
 
     }
 
-
     return (
         <div className={"detail-container"}>
             <div className={"main-container2-top"}>
-                <h2>카테고리 상세</h2>
+                <h2>메뉴 상세</h2>
             </div>
             <div className={"main-container2-body"}>
                 <div>
                     <div className={"add-input-part"}>
                         <span>이름</span>
-                        <input className={"m-input"} type={"text"} name={"name"} value={categoryDetail.name || ""}
+                        <input className={"m-input"} type={"text"} name={"name"} value={menuDetail.name || ""}
                                onChange={onChangeCategoryDetail}/>
                     </div>
                     <div className={"add-input-part position-left"} style={{
@@ -112,7 +116,7 @@ let CategoryDetail = ({category}) => {
                     }}>
                         <div>
                             <label className="switch">
-                                <input type="checkbox" name={"status"} checked={categoryDetail.status || false}
+                                <input type="checkbox" name={"status"} checked={menuDetail.status || false}
                                        onChange={onChangeCategoryStatusToggle}/>
                                 <span className="slider round"></span>
                             </label>
@@ -135,7 +139,7 @@ let CategoryDetail = ({category}) => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default CategoryDetail;
+export default MenuDetail;
