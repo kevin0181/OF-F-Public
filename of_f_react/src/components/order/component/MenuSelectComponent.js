59b621e067bd2efab3998d3617745bc64b16a705
유영빈(집) <kevin018111@gmail.com>
2023-02-01 20:16:28 +0900

    메뉴 선택했을때 안에 이미지 슬라이드로 했음

--- of_f_react/src/components/order/component/MenuSelectComponent.js
+++ of_f_react/src/components/order/component/MenuSelectComponent.js
@@ -5,7 +5,7 @@ import {
 } from "../../../store/order/orderSelectMenu";
 import {ReactComponent as XBtn} from "./../../../assets/icon/x.svg";
 import {orderStore as orderStoreRecoil} from "../../../store/order/orderStore";
-import SlideImg from "./SlideImg";
+import {useEffect, useState} from "react";
 
 let MenuSelectComponent = () => {
 
@@ -15,6 +15,29 @@ let MenuSelectComponent = () => {
     const [clickMenuStatus, setClickMenuStatus] = useRecoilState(clickMenuStatusRecoil); // 메뉴 선택시, 모달창 나옴
 
     const [selectOrderMenu, setSelectOrderMenu] = useRecoilState(selectOrderMenuRecoil); //  현재 선택된 카테고리의 메뉴 가져옴
+
+    const [imgCurrent, setImgCurrent] = useState(0); //이미지 번호
+    const [imgMoveStyle, setImgMoveStyle] = useState({
+        transform: `translate(-${imgCurrent}00%)`
+    })
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (selectOrderMenu.storeMenuImgs.length - 1 <= imgCurrent) {
+                setImgCurrent(0);
+            } else {
+                setImgCurrent(imgCurrent + 1);
+            }
+            console.log(imgCurrent)
+        }, 4000);
+        return (
+            setImgMoveStyle(
+                {
+                    transform: `translate(-${imgCurrent}00%)`
+                }
+            )
+        )
+    }, [imgCurrent]);
 
     return (
         <div className={"order-container animate__animated animate__slideInRight"}>
@@ -28,8 +51,21 @@ let MenuSelectComponent = () => {
                         </div>
                     </div>
                     <div className={"menu-select-img"}>
-                        <div>
-                            <SlideImg/>
+                        <div style={{
+                            width: "78%",
+                            height: "90%",
+                            overflow: "hidden"
+                        }}>
+                            <div style={imgMoveStyle}>
+                                {
+                                    selectOrderMenu.storeMenuImgs.map((data, index) => (
+                                        <img key={index}
+                                            // className={"" + (index === imgCurrent ? 'img-active' : '')}
+                                             src={`${process.env.REACT_APP_SERVER_URL_PORT}/api/v1/img/get?name=${data.name}&kind=menu&store=${orderStore.name}`}
+                                        />
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
