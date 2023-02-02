c22cca32ca157d5695a4bf48215716c20543882d
유영빈(집) <kevin018111@gmail.com>
2023-02-02 14:15:27 +0900

    바스켓 아이콘 추가완료

--- of_f_react/src/components/order/component/OrderFooter.js
+++ of_f_react/src/components/order/component/OrderFooter.js
@@ -1,7 +1,18 @@
+import {ReactComponent as Basket} from "./../../../assets/icon/shopping-cart.svg";
+
 let OrderFooter = () => {
     return (
         <div className={"order-container-footer"}>
-
+            <div className={"order-footer-basket"}>
+                <div>
+                    <Basket/>
+                </div>
+            </div>
+            <div className={"order-footer-total-order-btn"}>
+                <div className={"order-footer-btn"}>
+                    <p>1000원 결제 및 주문하기</p>
+                </div>
+            </div>
         </div>
     )
 }
