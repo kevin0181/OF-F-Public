071a4620574ac6a69f7f4f5f657ff6d71da4ad42
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-02-06 15:48:16 +0900

    주문 목록 보여주는 거 하는중 header 했음

--- of_f_react/src/components/order/component/MenuBasketList.js
+++ of_f_react/src/components/order/component/MenuBasketList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {ReactComponent as XBtn} from "../../../assets/icon/x.svg";
 import {useNavigate} from "react-router-dom";
+import "./../../../styles/css/order/basket.css";
 
 let MenuBasketList = () => {
     const navigate = useNavigate();
@@ -8,18 +9,25 @@ let MenuBasketList = () => {
     return (
         <div className={"order-container animate__animated animate__slideInUp"}>
             <div className={"menu-select-container"}>
-                <div className={"menu-select-container-header"}>
-                    <div className={"menu-select-x-btn"}>
+                <div className={"basket-list-container-header"}>
+                    <div className={"menu-select-x-btn"} style={{
+                        height: "auto"
+                    }}>
                         <div onClick={() => {
                             navigate(-1);
                         }}>
                             <XBtn/>
                         </div>
                     </div>
+                    <div className={"basket-title"}>
+                       <div>
+                           <h3>주문 목록</h3>
+                       </div>
+                    </div>
                 </div>
-                <div className={"menu-select-container-body"}>
+                <div className={"basket-list-container-body"}>
                 </div>
-                <div className={"menu-select-container-footer"}>
+                <div className={"basket-list-container-footer"}>
                 </div>
             </div>
         </div>
