5035511d664a7752f1b0138d67593ce13e9844df
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-02-06 17:20:37 +0900

    주문 목록에서 메뉴 변경 성공

--- of_f_react/src/components/order/component/MenuBasketList.js
+++ of_f_react/src/components/order/component/MenuBasketList.js
@@ -5,6 +5,11 @@ import "./../../../styles/css/order/basket.css";
 import {useRecoilState} from "recoil";
 import {order as orderRecoil} from "../../../store/order/order";
 import {orderStore as orderStoreRecoil} from "../../../store/order/orderStore";
+import {
+    clickMenuStatus as clickMenuStatusRecoil,
+    selectOrderMenu as selectOrderMenuRecoil
+} from "../../../store/order/orderSelectMenu";
+import MenuSelectComponent from "./MenuSelectComponent";
 
 let MenuBasketList = () => {
 
@@ -14,6 +19,8 @@ let MenuBasketList = () => {
     const [orderStore, setOrderStore] = useRecoilState(orderStoreRecoil); // 가게 정보
 
     const [order, setOrder] = useRecoilState(orderRecoil); //  주문 목록(장바구니)
+    const [selectOrderMenu, setSelectOrderMenu] = useRecoilState(selectOrderMenuRecoil); //  현재 선택된 메뉴 가져옴
+    const [clickMenuStatus, setClickMenuStatus] = useRecoilState(clickMenuStatusRecoil); // 메뉴 선택시, 모달창 나옴
 
     useEffect(() => {
         if (order.storeOrderMenus.length === 0) {
@@ -50,83 +57,93 @@ let MenuBasketList = () => {
     }, [order.storeOrderMenus]); // 주문 목록에서 메뉴가 변경된다면?
 
     return (
-        <div className={"order-container animate__animated animate__slideInUp"}>
-            <div className={"menu-select-container"}>
-                <div className={"basket-list-container-header"}>
-                    <div className={"menu-select-x-btn"} style={{
-                        height: "auto"
-                    }}>
-                        <div onClick={() => {
-                            navigate(-1);
-                        }}>
-                            <XBtn/>
+        <>
+            {
+                clickMenuStatus ? (
+                    <MenuSelectComponent/>
+                ) : (<div className={"order-container animate__animated animate__slideInUp"}>
+                    <div className={"menu-select-container"}>
+                        <div className={"basket-list-container-header"}>
+                            <div className={"menu-select-x-btn"} style={{
+                                height: "auto"
+                            }}>
+                                <div onClick={() => {
+                                    navigate(-1);
+                                }}>
+                                    <XBtn/>
+                                </div>
+                            </div>
+                            <div className={"basket-title"}>
+                                <div>
+                                    <h3>주문 목록</h3>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className={"basket-title"}>
-                        <div>
-                            <h3>주문 목록</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className={"basket-list-container-body"}>
-                    <div>
-                        {
-                            order.storeOrderMenus.map((menuData, menuIndex) => (
-                                <div className={"basket-part"} key={menuIndex}>
-                                    <div className={"basket-part-menu-title"}>
-                                        <div className={"basket-part-menu-h3"}>
-                                            <h3>{menuData.storeMenu.name}</h3>
-                                        </div>
-                                        <div className={"basket-part-menu-XBtn"} onClick={() => {
-                                            cancelOrderMenu(menuData)
-                                        }}>
-                                            <XBtn/>
-                                        </div>
-                                    </div>
-                                    <div className={"basket-part-menu-body"}>
-                                        <div className={"basket-part-img"}>
-                                            <div>
-                                                <img
-                                                    src={`${process.env.REACT_APP_SERVER_URL_PORT}/api/v1/img/get?name=${menuData.storeMenu.storeMenuImgs[0].name}&kind=menu&store=${orderStore.name}`}/>
+                        <div className={"basket-list-container-body"}>
+                            <div>
+                                {
+                                    order.storeOrderMenus.map((menuData, menuIndex) => (
+                                        <div className={"basket-part"} key={menuIndex}>
+                                            <div className={"basket-part-menu-title"}>
+                                                <div className={"basket-part-menu-h3"}>
+                                                    <h3>{menuData.storeMenu.name}</h3>
+                                                </div>
+                                                <div className={"basket-part-menu-XBtn"} onClick={() => {
+                                                    cancelOrderMenu(menuData)
+                                                }}>
+                                                    <XBtn/>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className={"basket-part-menu-info"}>
-                                            <div className={"basket-part-menu-info-price"}>
-                                                &bull;<small>메뉴 가격
-                                                : {menuData.size * Number(menuData.storeMenu.price)}원</small>
-                                            </div>
-                                            {
-                                                menuData.storeOrderSides.map((sideData, sideIndex) => (
-                                                    <div className={"basket-part-menu-info-side"} key={sideIndex}>
-                                                        &bull;<small>사이드
-                                                        : {sideData.storeSideMenu.name} - {sideData.storeSideMenu.price}원</small>
+                                            <div className={"basket-part-menu-body"}>
+                                                <div className={"basket-part-img"}>
+                                                    <div>
+                                                        <img
+                                                            src={`${process.env.REACT_APP_SERVER_URL_PORT}/api/v1/img/get?name=${menuData.storeMenu.storeMenuImgs[0].name}&kind=menu&store=${orderStore.name}`}/>
                                                     </div>
-                                                ))
-                                            }
-                                            <div className={"basket-part-menu-info-price"}>
-                                                &bull;<small>총 가격 : {menuData.price}원</small>
-                                            </div>
-                                            <div className={"basket-part-btn"}>
-                                                <div>
-                                                    변경
+                                                </div>
+                                                <div className={"basket-part-menu-info"}>
+                                                    <div className={"basket-part-menu-info-price"}>
+                                                        &bull;<small>메뉴 가격
+                                                        : {menuData.size * Number(menuData.storeMenu.price)}원</small>
+                                                    </div>
+                                                    {
+                                                        menuData.storeOrderSides.map((sideData, sideIndex) => (
+                                                            <div className={"basket-part-menu-info-side"}
+                                                                 key={sideIndex}>
+                                                                &bull;<small>사이드
+                                                                : {sideData.storeSideMenu.name} - {sideData.storeSideMenu.price}원</small>
+                                                            </div>
+                                                        ))
+                                                    }
+                                                    <div className={"basket-part-menu-info-price"}>
+                                                        &bull;<small>총 가격 : {menuData.price}원</small>
+                                                    </div>
+                                                    <div className={"basket-part-btn"}>
+                                                        <div onClick={() => {
+                                                            setSelectOrderMenu(menuData.storeMenu);
+                                                            setClickMenuStatus(true);
+                                                        }}>
+                                                            변경
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <div className={"basket-list-container-footer"}>
+                            <div>
+                                <p>
+                                    {order.totalPrice}원 결제 및 주문하기
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className={"basket-list-container-footer"}>
-                    <div>
-                        <p>
-                            {order.totalPrice}원 결제 및 주문하기
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+                </div>)
+            }
+        </>
     )
 }
 
