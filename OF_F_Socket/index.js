458ab7845bda84564281eab0f713a8d757f31654
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-02-17 13:59:03 +0900

    node서버에 mongodb연결 성공

--- OF_F_Socket/index.js
+++ OF_F_Socket/index.js
@@ -3,7 +3,25 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require("socket.io");
+const cors = require("cors");
 const PORT = 8000;
+const mongoose = require("mongoose");
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://off_user:kevin1141@localhost:27017/off', {dbName: 'off'}, function (err) {
+    // console.log(err);
+});
+
+// db 연결 실패
+mongoose.connection.on('error', function () {
+    console.error('db 연결 실패!');
+});
+
+// db 연결 성공
+mongoose.connection.once('open', function () {
+    console.log('db 연결 성공!');
+});
 
 const io = new Server(server, {
     cors: {
@@ -32,8 +50,11 @@ storeSpace.on('connection', (socket) => {
     });
 });
 
+app.use(cors({
+    origin: process.env.REACT_APP_PORT
+}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
